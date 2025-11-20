12a155ee83e4b9a4e7605143a7105ea1f513eff9
zainiss <41850412+zainiss@users.noreply.github.com>
2025-11-19 19:10:55 -0500

    migrate db and start making controllers

---
+++ BlogApp/BlogApp.API/Controllers/PostController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Core.Interfaces;
+using BlogApp.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostRepository _postRepo;
+
+        public PostsController(IPostRepository postRepo)
+        {
+            _postRepo = postRepo;
+        }
+
+        // GET: api/posts
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var posts = await _postRepo.GetAllAsync();
+            return Ok(posts);
+        }
+
+        // GET: api/posts/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var post = await _postRepo.GetByIdAsync(id);
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
+        }
+
+        // POST: api/posts
+        [HttpPost]
+        public async Task<IActionResult> Create(Post post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var createdPost = await _postRepo.CreateAsync(post);
+            return CreatedAtAction(nameof(GetById), new { id = createdPost.Id }, createdPost);
+        }
+
+        // PUT: api/posts/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Post post)
+        {
+            if (id != post.Id)
+                return BadRequest("ID in URL does not match ID in body.");
+
+            var updated = await _postRepo.UpdateAsync(post);
+
+            if (updated == null)
+                return NotFound();
+
+            return Ok(updated);
+        }
+
+        // DELETE: api/posts/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _postRepo.DeleteAsync(id);
+
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
--- BlogApp/BlogApp.API/Program.cs
+++ BlogApp/BlogApp.API/Program.cs
@@ -1,10 +1,28 @@
+using BlogApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using BlogApp.Core.Interfaces;
+using BlogApp.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
+
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddEndpointsApiExplorer();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -12,28 +30,4 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
