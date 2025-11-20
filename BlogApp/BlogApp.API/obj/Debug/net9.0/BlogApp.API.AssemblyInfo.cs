e2966d49011a7051f1907b8157ef45f27070672c
zainiss <41850412+zainiss@users.noreply.github.com>
2025-11-19 19:41:30 -0500

    postman working.

--- BlogApp/BlogApp.API/obj/Debug/net9.0/BlogApp.API.AssemblyInfo.cs
+++ BlogApp/BlogApp.API/obj/Debug/net9.0/BlogApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a155ee83e4b9a4e7605143a7105ea1f513eff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947322130ca2b63dd1dceb60445a7c48a4e896b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
